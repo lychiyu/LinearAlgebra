aaba2ce9da61172243fc3f0904aed036bfcff37a
ying liu <lychiyu@gmail.com>
2018-12-13 09:20:33 +0800

    vector

---
+++ playLA/Vector.py
@@ -0,0 +1,55 @@
+class Vector:
+    def __init__(self, l):
+        """使用列表初始化向量"""
+        self._values = list(l)
+
+    def __repr__(self):
+        return f"Vector({self._values})"
+
+    def __str__(self):
+        return f"({', '.join(str(e) for e in self._values)})"
+
+    def __len__(self):
+        """向量维度"""
+        return len(self._values)
+
+    def __getitem__(self, item):
+        """取出值"""
+        if item < len(self):
+            return self._values[item]
+        return None
+
+    def __iter__(self):
+        """返回向量的迭代器"""
+        return self._values.__iter__()
+
+    def __add__(self, other):
+        """向量加法"""
+        assert len(self) == len(other), '向量相加时维度必须相同'
+        return Vector([a + b for a, b in zip(self, other)])
+
+    def __sub__(self, other):
+        """向量减法"""
+        assert len(self) == len(other), '向量相减时维度必须相同'
+        return Vector([a - b for a, b in zip(self, other)])
+
+    def __mul__(self, k):
+        """向量数乘"""
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        """向量右乘,就是向量数乘"""
+        return self * k
+
+    def __pos__(self):
+        """向量取正的结果"""
+        return 1 * self
+
+    def __neg__(self):
+        """向量取负的结果"""
+        return -1 * self
+
+    @classmethod
+    def zero(cls, dim):
+        """返回dim维零向量"""
+        return cls([0] * dim)
---
+++ test/test_vector.py
@@ -0,0 +1,13 @@
+from playLA.Vector import Vector
+
+if __name__ == '__main__':
+    vec = Vector([3, 4])
+    print(vec)
+    print(len(vec))
+    print(vec[0], vec[1])
+
+    vec2 = Vector([2, 0])
+    print(f"{vec}+{vec2}={vec+vec2}")
+    print(f"{vec}-{vec2}={vec-vec2}")
+    print(f"{vec}*{3}={vec*3}")
+    print(Vector.zero(2))
